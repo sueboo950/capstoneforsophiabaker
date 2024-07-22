47f04bb81b9dc756750d1857e59b2cb498165497
sophie950 <sueboo950950@gmail.com>
2024-07-22 14:24:55 -0500

    update

--- Prayer/prayer.js
+++ Prayer/prayer.js
@@ -4,7 +4,7 @@ function createMessage(){
     message.request = prompt("What is your prayer request");
 
     prayerrequestParagraph.innerHTML = `Dear ${message.sender}, Thank you for your request! Here is your prayer below..`;
-    prayerrequestParagraph.innerHTML = `Dear God, we come to you today to interseed in prayer for ${message.sender}. We ask that you grant their request ${message.request}. We thank you heavenly father for hearing us and granting their request. In Jesus name, Amen!`;
+    prayerrequestParagraph.innerHTML = `Dear God, we come to you today to interseed in prayer for ${message.sender}. We ask that you grant their request for ${message.request}. We thank you heavenly father for hearing us and granting their request. In Jesus name, Amen!`;
 
 }
 prayerrequestButton.onclick = createMessage
